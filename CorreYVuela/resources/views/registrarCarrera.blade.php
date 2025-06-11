e8909974ed07578d303c89905b2a52eb1f15ac50
troya44 <guillermotroya129@gmail.com>
2025-06-11 12:15:02 +0200

    Finalizado

--- CorreYVuela/resources/views/registrarCarrera.blade.php
+++ CorreYVuela/resources/views/registrarCarrera.blade.php
@@ -9,7 +9,7 @@
     <link rel="shortcut icon" href="{{ asset('LogoCorreYVuela.png') }}" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <title>Registr  ar Carrera</title>
+    <title>Registrar Carrera</title>
 </head>
 
 <body>
